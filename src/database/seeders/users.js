39f4407b1363644e4b3aca9f7307998bdcb751a2
odunayo <okebunmiodunayo@gmail.com>
2020-07-17 00:18:51 +0100

    fixed test

--- src/database/seeders/users.js
+++ src/database/seeders/users.js
@@ -18,24 +18,28 @@ const createSeededUsers = async () => {
     username: faker.name.firstName(),
     email: faker.internet.email(),
     password: hashPassword(faker.internet.password()),
+    role: 'regular',
 
   });
   const user3 = new User({
     username: faker.name.firstName(),
     email: faker.internet.email(),
     password: hashPassword(faker.internet.password()),
+    role: 'regular',
 
   });
   const user4 = new User({
     username: faker.name.firstName(),
     email: faker.internet.email(),
     password: hashPassword(faker.internet.password()),
+    role: 'regular',
 
   });
   const user5 = new User({
     username: faker.name.firstName(),
     email: faker.internet.email(),
     password: hashPassword(faker.internet.password()),
+    role: 'regular',
 
   });
   await user1.save();
