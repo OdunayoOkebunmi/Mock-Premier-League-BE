f999dbe2f98cc215f6bff0e0d44a6ea38cf61fc7
odunayo <okebunmiodunayo@gmail.com>
2020-07-17 14:46:10 +0100

    fix rate limit

--- src/config/redisConfig.js
+++ src/config/redisConfig.js
@@ -33,7 +33,7 @@ export const checkRateLimit = (req, res, next) => {
       next();
     }
     if (difference < 1) {
-      if (req.session.user > MAX_WINDOW_REQUEST_COUNT) {
+      if (req.session.user.count > MAX_WINDOW_REQUEST_COUNT) {
         return errorResponse(res, 429, { message: 'Throttled limit exceeded. Please wait a moment and try again' });
       }
       req.session.user.count++;
